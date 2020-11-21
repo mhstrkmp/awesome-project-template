eb055fedec4ba8490dff498b5431d582b9c3c2c4
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-21 19:40:30 +0100

    :white_check_mark: Fix broken tests

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Page />
+      <Page
+        onLogin={() => console.log("login")}
+        onLogout={() => console.log("logout")}
+        onCreateAccount={() => console.log("create account")}
+      />
       <a href="/storybook">Go to Storybook</a>
     </div>
   );
